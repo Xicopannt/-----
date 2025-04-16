5d7d79b93aa8d25ee65f04d7f062015b7f88da60
healthy <113200436+Xicopannt@users.noreply.github.com>
2025-04-17 02:06:16 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const modal = document.getElementById('custommodal');
+const openModalBtn = document.getElementById('openModal');
+const closeModalBtn = document.getElementById('closeModal');
+
+function openModal(e) {
+  e.preventDefault();
+  modal.classList.add('show');
+}
+
+function closeModal() {
+  modal.classList.remove('show');
+}
+
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+window.addEventListener('click', function(e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target); 
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+document.querySelectorAll('.container > div').forEach(el => {
+  observer.observe(el);
+});
